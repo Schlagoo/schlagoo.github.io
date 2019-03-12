3bbb2b9c86ad8a5c725cfb9f97e4d35f8c02968b
Schlagoo <schlaak.pascal@gmail.com>
2019-03-12 11:24:51 +0100

    Added web portfolio to git

---
+++ main.js
@@ -0,0 +1,20 @@
+// $(document).ready(function(){    
+
+//     var scroll_pos = 0;
+//     // var topPos = document.getElementById("project-1").offsetTop;
+
+//     $(document).scroll(function() { 
+//         scroll_pos = $(this).scrollTop();
+
+//         if(scroll_pos > 50) {
+//             $("#project-1").css("border-bottom","4px solid black");
+//             //$("nav").css('background-color', 'black');
+//             // $("nav").css('color', 'white');
+//             // $("nav a").css('color', 'white');
+//         } else {
+//             // $("nav").css('background-color', 'white');
+//             // $("nav").css('color', 'black');
+//             // $("nav a").css('color', 'black'); 
+//         }
+//     });
+// });
