4cbe6a059894bf8d854a1f17f7471babaf9cc45a
Schlagoo <schlaak.pascal@gmail.com>
2019-05-25 20:34:43 +0200

    Fixed project overlay and started with project 3 site

--- mobile/main.js
+++ mobile/main.js
@@ -2,12 +2,27 @@
  * Function to open navbar
  */
 function open_nav(x) {
-    x.classList.toggle("change");
+
+  x.classList.toggle("change");
     
-    var y = document.getElementById("my_nav_links");
-    if (y.style.display === "flex") {
-      y.style.display = "none";
-    } else {
-      y.style.display = "flex";
-    }
+  var y = document.getElementById("my_nav_links");
+
+  if (y.style.display === "flex") {
+    y.style.display = "none";
+  } else {
+    y.style.display = "flex";
+  }
+}
+
+function close_nav() {
+
+  var h = document.getElementById("ham");
+  h.classList.toggle("rechange");
+  
+  var y = document.getElementById("my_nav_links");
+  if (y.style.display === "flex") {
+    y.style.display = "none";
+  } else {
+    y.style.display = "flex";
+  }
 }
