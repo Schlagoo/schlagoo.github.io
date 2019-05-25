772fe8f44b7216fdac02be3ad3f3913cc7726dc3
Schlagoo <schlaak.pascal@gmail.com>
2019-05-25 13:29:00 +0200

    Added fixed dropout navbar

--- mobile/main.js
+++ mobile/main.js
@@ -3,4 +3,11 @@
  */
 function open_nav(x) {
     x.classList.toggle("change");
+    
+    var y = document.getElementById("my_nav_links");
+    if (y.style.display === "flex") {
+      y.style.display = "none";
+    } else {
+      y.style.display = "flex";
+    }
 }
