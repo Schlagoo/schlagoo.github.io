8b3e674eff4d7accd65de6bdf325e4bc617b0143
Schlagoo <schlaak.pascal@gmail.com>
2020-02-17 10:24:51 +0100

    Added project 8 to project page as placeholder

--- main.js
+++ main.js
@@ -7,9 +7,9 @@ function handle_menu() {
 
   var menu = document.getElementById("menu");
 
-    if (menu.style.display === "none") {
-      menu.style.display = "block";
-    } else {
-      menu.style.display = "none";
-    }
+  if (menu.style.display === "none") {
+    menu.style.display = "block";
+  } else {
+    menu.style.display = "none";
+  }
 }
