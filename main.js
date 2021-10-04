d2ea235e8de2d2e25aa66b610962fb9391bb275b
Schlagoo <schlaak.pascal@gmail.com>
2021-10-04 17:06:44 +0200

    update: resume and intro

--- main.js
+++ main.js
@@ -54,7 +54,7 @@ function show_description(idx) {
   * idx = name of clicked element
   */
 
-  var elements = ["cadflex", "master", "bachelor", "bmw", "daimler", "mapal", "abitur", "forum"];
+  var elements = ["mercedes", "cadflex", "master", "bachelor", "bmw", "daimler", "mapal", "abitur", "forum"];
 
   // Set color to standard and remove all description-elements
   for (var i = 0; i < elements.length; i++) {
